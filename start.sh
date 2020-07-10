e392b5b32ba1924a471f08664059170be35da42c
Mahmed-ping <66841747+Mahmed-ping@users.noreply.github.com>
2020-07-10 08:51:30 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,46 @@
+#start.sh/dat.
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+
+    pkg update
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+    
+    pkg upgrade
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+   
+    pkg install git
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+    
+    pkg install python 
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+    
+    pkg install python2
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+    
+    pkg install php 
+echo "--------------------------------------------------"
+echo "|              YOUTUBE.COM/X2_INTERNET            |"
+echo "|              INSTAGRAM.COM/X2_INTERNET          |"
+echo "-------------------------------------------------"
+    
+    pkg install wget 
+
+echo "| ❤❤❤❤ Good py ❤❤❤❤ |"
+
